1522259d6bc9bd9d90822e65ef362243bb7e1d2e
yohdev-glory <glory@yohdev.com>
2023-11-15 02:01:29 -0500

    install localazy

--- config/plugins.js
+++ config/plugins.js
@@ -1,27 +1,27 @@
 module.exports = ({ env }) => ({
     // ...
-    translate: {
-        enabled: true,
-        config: {
-            // Add the name of your provider here (for example 'deepl' for strapi-provider-translate-deepl or the full package name)
-            provider: 'deepl',
-            providerOptions: {
-                // Your provider might define some custom options like an apiKey
-                apiKey: env('DEEPL_API_KEY')
-            },
-            // Which field types are translated (default string, text, richtext, components and dynamiczones)
-            // Either string or object with type and format
-            // Possible formats: plain, markdown, html (default plain)
-            translatedFieldTypes: [
-                'string',
-                { type: 'text', format: 'plain' },
-                { type: 'richtext', format: 'markdown' },
-                'component',
-                'dynamiczone',
-            ],
-            // If relations should be translated (default true)
-            translateRelations: true,
-        },
-    },
+    // translate: {
+    //     enabled: true,
+    //     config: {
+    //         // Add the name of your provider here (for example 'deepl' for strapi-provider-translate-deepl or the full package name)
+    //         provider: 'deepl',
+    //         providerOptions: {
+    //             // Your provider might define some custom options like an apiKey
+    //             apiKey: env('DEEPL_API_KEY')
+    //         },
+    //         // Which field types are translated (default string, text, richtext, components and dynamiczones)
+    //         // Either string or object with type and format
+    //         // Possible formats: plain, markdown, html (default plain)
+    //         translatedFieldTypes: [
+    //             'string',
+    //             { type: 'text', format: 'plain' },
+    //             { type: 'richtext', format: 'markdown' },
+    //             'component',
+    //             'dynamiczone',
+    //         ],
+    //         // If relations should be translated (default true)
+    //         translateRelations: true,
+    //     },
+    // },
     // ...
 })
